95d1238a08f7441d4123c639dc0b3c6aefc3e864
VANN YA <vannakkm33@gmail.com>
2024-03-31 18:31:01 +0700

    Add react-router-dom dependency and implement routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-import Navebar from './components/Neavbar'
+import Navebar from "./components/Neavbar";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Product from "./pages/Product";
+import ProductDetail from "./pages/ProductDetail";
+import Search from "./pages/Search";
 function App() {
   return (
-    <>
-    {/* Navbar */}
-      <Navebar/>
-      {/* contener */}
-      <h1>Hello, world!</h1>
-      <p>Welcome to your new single-spa application.</p>
-      {/* Footer */}
-      <Footer/>
-    </>
+    <div>
+      <Navebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/product/search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notfoud() {
+  return (
+    <div>Notfoud</div>
+  )
+}
+
+export default Notfoud
